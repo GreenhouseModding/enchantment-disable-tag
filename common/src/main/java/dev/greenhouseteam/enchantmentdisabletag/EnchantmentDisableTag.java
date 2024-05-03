ee28ebbbb8c7c44c94050a5c94d09ea0ccfa93b0
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-04 07:28:51 +1000

    Invalidate enchantments on items if they are somehow obtained in a client context.

--- common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/EnchantmentDisableTag.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/EnchantmentDisableTag.java
@@ -1,14 +1,44 @@
 package dev.greenhouseteam.enchantmentdisabletag;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
+
+import java.util.Map;
 
 public class EnchantmentDisableTag {
     public static final String MOD_ID = "enchantmentdisabletag";
 
     public static final TagKey<Enchantment> DISABLED_ENCHANTMENT_TAG = TagKey.create(Registries.ENCHANTMENT, asResource("disabled"));
+
+    public static void removeDisabledEnchantments(ItemStack stack) {
+        if (stack.has(DataComponents.ENCHANTMENTS)) {
+            ItemEnchantments.Mutable itemEnchantments = new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
+            for (Map.Entry<Holder<Enchantment>, Integer> entry : stack.getOrDefault(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY).entrySet())
+                if (entry.getKey().isBound() && !entry.getKey().is(EnchantmentDisableTag.DISABLED_ENCHANTMENT_TAG))
+                    itemEnchantments.set(entry.getKey().value(), entry.getValue());
+            if (itemEnchantments.keySet().isEmpty())
+                stack.remove(DataComponents.ENCHANTMENTS);
+            else
+                stack.set(DataComponents.ENCHANTMENTS, itemEnchantments.toImmutable());
+        }
+        if (stack.has(DataComponents.STORED_ENCHANTMENTS)) {
+            ItemEnchantments.Mutable itemEnchantments = new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
+            for (Map.Entry<Holder<Enchantment>, Integer> entry : stack.getOrDefault(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY).entrySet())
+                if (entry.getKey().isBound() && !entry.getKey().is(EnchantmentDisableTag.DISABLED_ENCHANTMENT_TAG))
+                    itemEnchantments.set(entry.getKey().value(), entry.getValue());
+            if (itemEnchantments.keySet().isEmpty())
+                stack.set(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY);
+            else
+                stack.set(DataComponents.STORED_ENCHANTMENTS, itemEnchantments.toImmutable());
+        }
+    }
 
     public static ResourceLocation asResource(String path) {
         return new ResourceLocation(MOD_ID, path);
---
+++ common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/mixin/SlotMixin.java
@@ -0,0 +1,19 @@
+package dev.greenhouseteam.enchantmentdisabletag.mixin;
+
+import dev.greenhouseteam.enchantmentdisabletag.EnchantmentDisableTag;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Slot.class)
+public class SlotMixin {
+    @ModifyVariable(method = "set", at = @At("HEAD"), argsOnly = true)
+    private ItemStack enchantmentdisabletag$removeDisabledEnchantments(ItemStack stack) {
+        EnchantmentDisableTag.removeDisabledEnchantments(stack);
+        return stack;
+    }
+}
