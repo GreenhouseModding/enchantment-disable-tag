201549c210446f80ee24fb933f9f87d6ff053e29
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-04 06:37:51 +1000

    Fix mod id field.

--- common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/EnchantmentDisableTag.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/EnchantmentDisableTag.java
@@ -4,12 +4,9 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.enchantment.Enchantment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class EnchantmentDisableTag {    public static final String MOD_ID = "enchantmentdisabledtag";
-    public static final String MOD_NAME = "Enchantment Disabled Tag";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+public class EnchantmentDisableTag {
+    public static final String MOD_ID = "enchantmentdisabletag";
 
     public static final TagKey<Enchantment> DISABLED_ENCHANTMENT_TAG = TagKey.create(Registries.ENCHANTMENT, asResource("disabled"));
 
