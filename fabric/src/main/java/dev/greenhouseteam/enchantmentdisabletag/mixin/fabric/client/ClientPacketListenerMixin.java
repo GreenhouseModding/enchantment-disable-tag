831292c1eee2bff0104ba33d2f88a7a5e56a5dca
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-04 18:17:29 +1000

    Fix compile errors.

--- fabric/src/main/java/dev/greenhouseteam/enchantmentdisabletag/mixin/fabric/client/ClientPacketListenerMixin.java
+++ fabric/src/main/java/dev/greenhouseteam/enchantmentdisabletag/mixin/fabric/client/ClientPacketListenerMixin.java
@@ -1,17 +1,17 @@
 package dev.greenhouseteam.enchantmentdisabletag.mixin.fabric.client;
 
 import dev.greenhouseteam.enchantmentdisabletag.EnchantmentDisableTag;
-import net.minecraft.client.multiplayer.TagCollector;
-import net.minecraft.core.RegistryAccess;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ClientboundUpdateTagsPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TagCollector.class)
-public class TagCollectorMixin {
-    @Inject(method = "updateTags", at = @At("TAIL"))
-    private void enchantmentdisabletag$updateTagValue(RegistryAccess registryAccess, boolean bl, CallbackInfo ci) {
+@Mixin(ClientPacketListener.class)
+public class ClientPacketListenerMixin {
+    @Inject(method = "handleUpdateTags", at = @At("TAIL"))
+    private void enchantmentdisabletag$updateTagValue(ClientboundUpdateTagsPacket packet, CallbackInfo ci) {
         EnchantmentDisableTag.setReloaded();
     }
 }
