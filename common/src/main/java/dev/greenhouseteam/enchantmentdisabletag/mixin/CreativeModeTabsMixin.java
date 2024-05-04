e00273c418282cef3aa543d4837060bdaf430ff3
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-04 18:06:44 +1000

    Fix CreativeModeTabsMixin.

--- common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/mixin/CreativeModeTabsMixin.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/mixin/CreativeModeTabsMixin.java
@@ -20,6 +20,6 @@ public class CreativeModeTabsMixin {
 
     @ModifyExpressionValue(method = "tryRebuildTabContents", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/CreativeModeTab$ItemDisplayParameters;needsUpdate(Lnet/minecraft/world/flag/FeatureFlagSet;ZLnet/minecraft/core/HolderLookup$Provider;)Z"))
     private static boolean enchantmentdisabletag$refreshTabContents(boolean original, FeatureFlagSet flagSet, boolean hasPermissions, HolderLookup.Provider provider) {
-        return original && EnchantmentDisableTag.getAndResetReloadState();
+        return original || EnchantmentDisableTag.getAndResetReloadState();
     }
 }
