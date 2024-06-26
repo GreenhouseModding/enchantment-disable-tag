acaab328647b8526c7c8e4a1a51832edf0246710
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-06-26 12:14:11 +1000

    Fix EnchantmentHelperMixin operating on immutable maps.

--- common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/mixin/EnchantmentHelperMixin.java
+++ common/src/main/java/dev/greenhouseteam/enchantmentdisabletag/mixin/EnchantmentHelperMixin.java
@@ -7,13 +7,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
     @ModifyVariable(method = "setEnchantments", at = @At("HEAD"), argsOnly = true)
     private static Map<Enchantment, Integer> enchantmentdisabletag$removeFromEnchantmentSetting(Map<Enchantment, Integer> original) {
-        original.keySet().removeIf(enchantment -> EnchantmentDisableTag.getHolder(enchantment).is(EnchantmentDisableTag.DISABLED_ENCHANTMENT_TAG));
+        Map<Enchantment, Integer> newMap = new HashMap<>(original);
+        newMap.keySet().removeIf(enchantment -> EnchantmentDisableTag.getHolder(enchantment).is(EnchantmentDisableTag.DISABLED_ENCHANTMENT_TAG));
         return original;
     }
 }
