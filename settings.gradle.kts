099a2694bdae5139f6941d59134e477d3bd9cddc
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-03-17 09:18:26 +1100

    refactor: Forgejo-ify build script.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,5 @@ plugins {
 
 // This should match the folder name of the project, or else IDEA may complain (see https://youtrack.jetbrains.com/issue/IDEA-317606)
 rootProject.name = "enchantment-disable-tag"
-include("xplat")
-include("fabric")
-include("neoforge")
+includeBuild("build_logic")
+include("xplat", "fabric", "neoforge")
